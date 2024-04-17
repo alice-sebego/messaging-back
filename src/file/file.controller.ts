43c47f3e449c6183ed1d4e8b855158a4869db68b
Alice Sebego <alice.etvotrecom@gmail.com>
2024-04-17 14:37:59 +0200

    test: upload file

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { IFile } from './Model/file';
 
 @Controller('file')
 export class FileController {
   constructor() {}
   @Post('/upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
+  uploadFile(@UploadedFile() file: Express.Multer.File): IFile {
     console.log(file);
-    return `This is the ${file.originalname}`;
+    //return `This is the ${file.originalname}`;
+    return file;
   }
 }
